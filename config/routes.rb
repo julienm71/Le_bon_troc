7e54e39b08ebd8db1340fba0b53fe6c535e67677
Julien Martin <julienm.contact@gmail.com>
2019-05-10 10:03:52 +0200

    conversation base

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
     resources :conflicts, only: [:index, :update, :destroy, :edit]
     get 'users/modal_settings', to: 'users#modal_settings'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :conversations do
+    resources :messages
+  end
 end
